268cca692fce581719bfae45ac64e3de997751d6
xiaonian0430 <xiaonian0430@163.com>
2021-11-02 14:55:49 +0800

    优化启动文件

--- run.php
+++ run.php
@@ -5,32 +5,12 @@
  * @contact: xiaonian030@163.com
  * @datetime: 2021-09-14 10:00
  */
-use Workerman\Worker;
 use HP\Http\App;
 
 //初始化
 ini_set('display_errors', 'on');
 defined('IN_PHAR') or define('IN_PHAR', boolval(\Phar::running(false)));
 defined('SERVER_ROOT') or define('SERVER_ROOT', IN_PHAR ? \Phar::running() : realpath(getcwd()));
-defined('PUBLIC_ROOT') or define('PUBLIC_ROOT', SERVER_ROOT.'/public'); //不能加后缀
-
-//创建临时目录
-$temp_path=SERVER_ROOT.'/temp';
-$log_path=SERVER_ROOT.'/temp/log';
-if(!is_dir($log_path)){
-    mkdir($log_path, 0777, true);
-}
-
-// 检查扩展或环境
-if(strpos(strtolower(PHP_OS), 'win') === 0) {
-    exit("start.php not support windows.\n");
-}
-if(!extension_loaded('pcntl')) {
-    exit("Please install pcntl extension.\n");
-}
-if(!extension_loaded('posix')) {
-    exit("Please install posix extension.\n");
-}
 
 //自动加载文件
 $auto_file=SERVER_ROOT . '/vendor/autoload.php';
@@ -40,42 +20,8 @@ if (file_exists($auto_file)) {
     exit("Please composer install.\n");
 }
 
+//初始化应用
+$app = new App();
 
-//导入配置文件
-$mode='produce';
-foreach ($argv as $item){
-    $item_val=explode('=', $item);
-    if(count($item_val)==2 && $item_val[0]=='-mode'){
-        $mode=$item_val[1];
-    }
-}
-$config_path=SERVER_ROOT . '/config/'.$mode.'.php';
-if (file_exists($config_path)) {
-    $conf = require_once $config_path;
-}else{
-    exit($config_path." is not exist\n");
-}
-defined('CONFIG') or define('CONFIG', $conf);
-
-//初始化worker
-Worker::$stdoutFile = $log_path.'/error.log';
-Worker::$logFile = $log_path.'/log.log';
-Worker::$pidFile = $temp_path.'/pid.pid';
-
-//实例化
-$address='http://'.CONFIG['HTTP_SERVER']['LISTEN_ADDRESS'].':'.CONFIG['HTTP_SERVER']['PORT'];
-$http_server = new Worker($address);
-
-//进程名称
-$http_server->name= CONFIG['HTTP_SERVER']['SERVER_NAME'];
-
-// 进程数量
-$http_server->count = CONFIG['HTTP_SERVER']['PROCESS_COUNT'];
-
-// 接收请求
-$http_server->onMessage = function ($connection, $request) {
-    new App($connection, $request);
-};
-
-// 启动服务
-Worker::runAll();
+//启动
+$app->run();
