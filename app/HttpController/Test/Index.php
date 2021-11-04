d984e6715d06dc95f1187a77b772ccaa207a3109
xiaonian0430 <xiaonian0430@163.com>
2021-11-04 16:10:27 +0800

    优化实例

--- app/HttpController/Test/Index.php
+++ app/HttpController/Test/Index.php
@@ -7,6 +7,7 @@
 
 namespace App\HttpController\Test;
 use App\HttpController\Basic;
+use HP\Database\MysqliDb;
 
 class Index extends Basic
 {
@@ -14,13 +15,6 @@ class Index extends Basic
     {
         $s = microtime(true);
         $used_time = (microtime(true) - $s);
-        $options = [
-            'parameters' => [
-                'database' => 10,
-            ],
-        ];
-        $client = new \Predis\Client('tcp://120.24.187.47:51012', $options);
-        $client->set('test', 12);
         $this->writeJson(200, ['api'=>1, 'used_time'=>$used_time], '吃了');
     }
 
@@ -32,6 +26,20 @@ class Index extends Basic
 
     public function redisTest()
     {
+        $s = microtime(true);
+        $options = [
+            'parameters' => [
+                'database' => 10,
+            ],
+        ];
+        $client = new \Predis\Client('tcp://120.24.187.47:51012', $options);
+        $client->set('test', 12);
+        $used_time = (microtime(true) - $s);
+        $this->writeJson(200, ['redisTest'=>1, 'used_time'=>$used_time], '吃了');
+    }
+
+    public function redisTestPatch()
+    {
         \Swoole\Runtime::enableCoroutine(true, $flags = SWOOLE_HOOK_ALL);
         $s = microtime(true);
         for($i=0;$i<800;$i++){
@@ -42,10 +50,68 @@ class Index extends Basic
                     ],
                 ];
                 $client = new \Predis\Client('tcp://120.24.187.47:51012', $options);
-                $client->set('test_12_'.$i, 12);
+                $client->set('redisTestPatch_'.$i, 12);
             });
         }
         $used_time = (microtime(true) - $s);
-        $this->writeJson(200, ['api'=>1, 'used_time'=>$used_time], '吃了');
+        $this->writeJson(200, ['redisTestPatch'=>1, 'used_time'=>$used_time], '吃了');
+    }
+
+    public function mysqlTest()
+    {
+        /**
+        CREATE TABLE `inter_test` (
+        `id` int(8) unsigned NOT NULL AUTO_INCREMENT,
+        `name` varchar(100) NOT NULL,
+        PRIMARY KEY (`id`)
+        ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
+         */
+        \Swoole\Runtime::enableCoroutine(true, $flags = SWOOLE_HOOK_ALL);
+        $s = microtime(true);
+        go(function () {
+            $database = new MysqliDb (Array (
+                'host' => '120.25.72.119',
+                'username' => 'root',
+                'password' => 'Kn6Bl4hVTkovGmcECSDR',
+                'db'=> 'test_demo',
+                'port' => 52341,
+                'prefix' => '',
+                'charset' => 'utf8mb4'));
+            $database->insert('inter_test', [
+                'name' => 'mysqlTest_1212'
+            ]);
+        });
+        $used_time = (microtime(true) - $s);
+        $this->writeJson(200, ['mysqlTest'=>1, 'used_time'=>$used_time], '吃了');
+    }
+
+    public function mysqlTestPatch()
+    {
+        /**
+        CREATE TABLE `inter_test` (
+        `id` int(8) unsigned NOT NULL AUTO_INCREMENT,
+        `name` varchar(100) NOT NULL,
+        PRIMARY KEY (`id`)
+        ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
+         */
+        \Swoole\Runtime::enableCoroutine(true, $flags = SWOOLE_HOOK_ALL);
+        $s = microtime(true);
+        for($i=0;$i<100;$i++){
+            go(function () use($i) {
+                $database = new MysqliDb (Array (
+                    'host' => '120.25.72.119',
+                    'username' => 'root',
+                    'password' => 'Kn6Bl4hVTkovGmcECSDR',
+                    'db'=> 'test_demo',
+                    'port' => 52341,
+                    'prefix' => '',
+                    'charset' => 'utf8mb4'));
+                $database->insert('inter_test', [
+                    'name' => 'mysqlTestPatch_'.$i
+                ]);
+            });
+        }
+        $used_time = (microtime(true) - $s);
+        $this->writeJson(200, ['mysqlTestPatch'=>1, 'used_time'=>$used_time], '吃了');
     }
 }
