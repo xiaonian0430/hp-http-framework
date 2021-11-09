f0d10675258fd86bf554a5545f314beb8fedf7a1
xiaonian0430 <xiaonian0430@163.com>
2021-11-09 10:42:08 +0800

    Update Index.php

--- app/Task/Index.php
+++ app/Task/Index.php
@@ -6,10 +6,12 @@
  */
 
 namespace App\Task;
+use Workerman\Lib\Timer;
 use longlang\phpkafka\Consumer\Consumer;
 use longlang\phpkafka\Consumer\ConsumerConfig;
 class Index
 {
+    private $consumer;
     public function action()
     {
         echo 'test'.PHP_EOL;
@@ -20,10 +22,23 @@ class Index
         $config->setClientId('test'); // 客户端ID，不同的消费者进程请使用不同的设置
         $config->setGroupInstanceId('test'); // 分组实例ID，不同的消费者进程请使用不同的设置
         $config->setInterval(0.1);
-        $consumer = new Consumer($config, function(ConsumeMessage $message){
-            var_dump($message->getKey() . ':' . $message->getValue());
-            // $consumer->ack($message); // autoCommit设为false时，手动提交
+        $config->setAutoCommit(false); //自动提交数据
+        $this->consumer = new Consumer($config);
+
+        // 每0.2秒执行一次
+        $time_interval = 0.2;
+        Timer::add($time_interval, function(){
+            $this->fetchData();
         });
-        $consumer->start();
+    }
+
+    private function fetchData(){
+        try {
+            $message = $this->consumer->consume();
+            if($message) {
+                var_dump($message->getKey() . ':' . $message->getValue());
+                $this->consumer->ack($message); // 手动提交
+            }
+        }catch (\Throwable $e){}
     }
 }
